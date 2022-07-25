330a6e5d62d50e44a7308f13f0ae2ef57aa55a35
garytsukla <gary@tsukla.com>
2022-07-25 04:34:27 +0100

    Added cache control

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const addPath=function(p){
-  return path.join(__dirname,p);
+const addPath = function (p) {
+  return path.join(__dirname, p);
+};
+const setCacheSend = function (r, seconds, file) {
+  r.set('Cache-Control', 'public, max-age=' + seconds);
+  r.sendFile(addPath(file));
 };
 
 // Send requested page, h=header&request,r=response
-app.get('/',(h,r)=>{
-  r.sendFile(addPath('/index.html'));
+app.get('/', (h, r) => {
+  setCacheSend(r, 86400, '/index.html');
 });
-app.get('/main.css',(h,r)=>{
-  r.sendFile(addPath('/main.css'));
+app.get('/main.css', (h, r) => {
+  setCacheSend(r, 86400, '/main.css');
 });
-app.get('/script.js',(h,r)=>{
-  r.sendFile(addPath('/script.js'));
+app.get('/script.js', (h, r) => {
+  setCacheSend(r, 86400, '/script.js');
 });
-app.get('/image/intro.svg',(h,r)=>{
-  r.sendFile(addPath('/image/intro.svg'));
+app.get('/image/intro.svg', (h, r) => {
+  setCacheSend(r, 2628000, '/image/intro.svg');
 });
 
 // Throw 404 if no page found
-app.use((h,r)=>{
+app.use((h, r) => {
   r.status(404).sendFile(addPath('/index.html'));
 });
 const PORT = process.env.PORT || 8080;
