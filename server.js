0650b9124b4005751054bd780edb6db01b7a2e4c
garytsukla <gary@tsukla.com>
2023-01-20 04:56:28 +0000

    added new project

--- server.js
+++ server.js
@@ -8,13 +8,21 @@ const setCacheSend = function (r, seconds, file) {
   r.set('Cache-Control', 'public, max-age=' + seconds);
   r.sendFile(addPath(file));
 };
-
 // Send requested page, h=header&request,r=response
 app.get('/', (h, r) => {
   setCacheSend(r, 86400, '/index.html');
 });
+app.get('/favouritethings', (h, r) => {
+  setCacheSend(r, 86400, '/favourite.html');
+});
+app.get('/favoritethings', (h, r) => {
+  setCacheSend(r, 86400, '/favourite.html');
+});
 app.get('/main.css', (h, r) => {
   setCacheSend(r, 86400, '/main.css');
+});
+app.get('/css/style.css', (h, r) => {
+  setCacheSend(r, 86400, '/favouriteStyle.css');
 });
 app.get('/script.js', (h, r) => {
   setCacheSend(r, 86400, '/script.js');
